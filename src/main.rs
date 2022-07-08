6661d447c3bf8f8a78f1039aa5e93f719184be9c
Will Bradley <williambbradley@gmail.com>
2022-07-08 14:05:05 -0600

    wip

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,10 @@ use std::fs;
 use std::str::from_utf8;
 use std::vec::Vec;
 
+#[derive(Debug, Copy, Clone)]
 pub struct ParseState<'a>(&'a [u8], usize);
 
+#[derive(Debug, Copy, Clone)]
 pub enum Progress<'a, V> {
     Parsed(ParseState<'a>, V),
     Failed,
@@ -28,39 +30,40 @@ pub fn digits(ps: ParseState) -> Progress<&str> {
     }
 }
 
-pub fn char(ch: u32) -> impl Fn(ParseState) -> Progress<u32> {
-    move |ps: ParseState| -> Progress<u32> {
+pub fn char(ch: u32) -> impl Fn(ParseState) -> Progress<&str> {
+    move |ps: ParseState| -> Progress<&str> {
         let ParseState(content, start_index) = ps;
         let content_len = content.len();
         let mut index = start_index;
         // TODO: handle utf-8
         if index < content_len && content[index] as u32 == ch {
             index += 1;
-            Progress::Parsed(ParseState(content, index), ch)
+            let str_ch = [ch];
+            Progress::Parsed(ParseState(content, index), from_utf8(&str_ch))
         } else {
             Progress::Failed
         }
     }
 }
 
-pub fn sequence<'a, T, P, U>(parsers : &Vec<P>) -> impl Fn(ParseState) -> Progress<Vec<T>>
-    where P: FnMut(ParseState) -> Progress<u32>,
-          U: IntoIterator {
-    move |ps: ParseState| -> Progress<Vec<T>> {
-        let mut nodes: Vec<T> = Vec::new();
+pub fn sequence<P>(parsers: &Vec<P>) -> impl Fn(ParseState) -> Progress<Vec<&str>> + Clone
+where
+    P: Fn(ParseState) -> Progress<&str> + Clone,
+{
+    let parsers = parsers.clone();
+    move |ps: ParseState| -> Progress<Vec<&str>> {
+        let mut nodes: Vec<&str> = Vec::new();
         let mut parse_state = ps;
-        for parser in parsers {
+        for parser in &parsers {
             match parser(parse_state) {
-                Parsed(next_parse_state, node) {
+                Progress::Parsed(next_parse_state, node) => {
                     parse_state = next_parse_state;
-                    nodes.append(node);
+                    nodes.push(node);
                 }
-                Failed {
-                    return Progress::Failed
-                }
+                Progress::Failed => return Progress::Failed,
             }
         }
-        Progress::Failed
+        Progress::Parsed(parse_state, nodes)
     }
 }
 
@@ -72,15 +75,10 @@ fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
     let buffer: String = fs::read_to_string(args[1].clone())?;
 
     let ps: ParseState = ParseState(buffer.as_bytes(), 0);
-    match digits(ps) {
+    let language_parser = sequence([digits, char('Z' as u32)]);
+    match language_parser(ps) {
         Progress::Parsed(_, v) => println!("found digits {v}"),
         Progress::Failed => println!("parse error!"),
-    }
-
-    let ps: ParseState = ParseState(buffer.as_bytes(), 0);
-    match char()(ps) {
-        Progress::Parsed(_, v) => println!("found char {v}"),
-        Progress::Failed => println!("parse error finding character!"),
     }
     Ok(())
 }
